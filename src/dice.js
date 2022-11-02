a5fe3e6f1c4b9b893a7e8b403c2d6555bfed8d04
Calcifer1001 <dseidlertoken@gmail.com>
2022-11-02 19:21:52 -0300

    [ADD] roll dice

--- src/dice.js
+++ src/dice.js
@@ -1,3 +1,11 @@
 function rollDie(faces) {
     return Math.floor(Math.random() * faces + 1)
 }
+
+function rollDice(faces, qty) {
+    let result = []
+    for (let i = 0; i < qty; i++) {
+        result.push(rollDie(faces))
+    }
+    return result
+}
